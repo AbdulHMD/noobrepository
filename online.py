7c95fe35b33ce1483636b723bbf763d535ccf370
Mongoose <31045303+Mongo-ose@users.noreply.github.com>
2020-02-28 08:52:23 +0300

    Update online.py

--- online.py
+++ online.py
@@ -2,7 +2,7 @@ import vk_api
 import time
 from time import gmtime, strftime
 
-token = "09c668c3c6f6a709851d71db122d9246260955274561f85bc1ffcbdb02bb1612c2d9901066315681738ea" # можно получить на vkhost.github.io
+token = "" # vkhost.github.io
 
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
